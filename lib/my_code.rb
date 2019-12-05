98641dbe38b6ddadec4ae0d01394dbd24e1c6a85
Jazz Bozner <jazzbozner@gmail.com>
2019-12-05 23:47:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,4 +12,11 @@ def reduce(array, sv = nil)
   if sv 
     num1 = sv
     counter = 0
+  else
+    num1 = array[0]
+    counter = 1
+  end
+  
+  while counter <array.length do
+    yield(num1, array[counter])
 end
